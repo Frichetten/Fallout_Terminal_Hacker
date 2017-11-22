1177262677a20b3e6a55ea92091a2dbfc6b73767
Nick Frichette <frichetten@gmail.com>
2017-11-21 23:12:44 -0600

    Started working on the permissions of the app. I've got the camera one loaded. Now I just need to pull from previous pictures (or I could not do that to respect the users privacy and require they take pictures within the app, although that can be a hassle for them)

--- Main/src/main/java/com/frichetten/falloutterminalhacker/Main/MainActivity.java
+++ Main/src/main/java/com/frichetten/falloutterminalhacker/Main/MainActivity.java
@@ -1,7 +1,17 @@
 package com.frichetten.falloutterminalhacker.Main;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+import android.support.design.widget.*;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int rc = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if (rc == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getApplicationContext(), "Successfully Loaded the Camera",
+                    Toast.LENGTH_LONG).show();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    1);
+
+        }
+
     }
 }
